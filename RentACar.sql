3154fa7da250d43e1d64781aef35ce7425c17a2d
fdoljanin <doljaninfrane@gmail.com>
2020-12-27 01:28:54 +0100

    Added more test data

--- RentACar.sql
+++ RentACar.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE RentACar;
---
+----- 
 USE RentACar;
 
 CREATE TABLE Employees(
@@ -10,8 +10,8 @@ CREATE TABLE Employees(
 	Salary int CHECK(Salary>0)
 );
 
-CREATE TABLE PriceClasses(
-	PriceClassId int IDENTITY(1,1) PRIMARY KEY,
+CREATE TABLE PricingClasses(
+	PricingClassId int IDENTITY(1,1) PRIMARY KEY,
 	ClassName varchar(100) NOT NULL UNIQUE,
 	WinterPrice int CHECK(WinterPrice>0) NOT NULL,
 	SummerPrice int CHECK(SummerPrice>0) NOT NULL
@@ -21,10 +21,10 @@ CREATE TABLE Vehicles(
 	VehicleId int IDENTITY(1,1) PRIMARY KEY,
 	Brand varchar(100) NOT NULL,
 	Model varchar(100) NOT NULL,
-	Type varchar(100) NOT NULL,
+	Type varchar(100) CHECK(Type IN ('Car','Van','Scooter')) NOT NULL,
 	Color varchar(30) NOT NULL,
-	PriceClassId int FOREIGN KEY REFERENCES PriceClasses(PriceClassId) NOT NULL,
-	Mileage int CHECK(Mileage>=0),
+	PricingClassId int FOREIGN KEY REFERENCES PricingClasses(PricingClassId) NOT NULL,
+	Mileage int CHECK(Mileage>=0) NOT NULL,
 	LastRegistration datetime2
 );
 
@@ -34,17 +34,18 @@ CREATE TABLE Registrations(
 	RegisterDate datetime2 NOT NULL,
 );
 
-
 GO
 CREATE TRIGGER UpdateRegistrations
 ON Registrations
 AFTER INSERT
 AS
 BEGIN 
-	UPDATE Vehicles 
+	UPDATE Vehicles
 	SET Vehicles.LastRegistration = (SELECT MAX(RegisterDate) FROM inserted WHERE inserted.VehicleId = Vehicles.VehicleId)
-END;
-
+	WHERE (SELECT MAX(RegisterDate) FROM inserted WHERE inserted.VehicleId = Vehicles.VehicleId) > Vehicles.LastRegistration 
+	OR Vehicles.LastRegistration IS NULL
+END
+GO
 
 CREATE TABLE RentPurchases(
 	RentId int IDENTITY(1,1) PRIMARY KEY,
@@ -53,15 +54,32 @@ CREATE TABLE RentPurchases(
 	CustomerFirstName varchar(100) NOT NULL,
 	CustomerLastName varchar(100) NOT NULL,
 	CustomerPin varchar(11) CHECK(ISNUMERIC(CustomerPin)=1) NOT NULL,
-	CustomerBirth datetime2 NOT NULL,
+	CustomerBirth datetime2 CHECK(DATEDIFF(yy, CustomerBirth, GETDATE()) >= 18) NOT NULL, 
 	CustomerLicense varchar(8) CHECK(ISNUMERIC(CustomerLicense)=1) NOT NULL,
 	CustomerCreditCard varchar(16) CHECK(ISNUMERIC(CustomerCreditCard)=1) NOT NULL,
 	TransactionDate datetime2 NOT NULL,
 	StartTime datetime2 NOT NULL,
-	Duration float(1) NOT NULL CHECK(Duration>=1)
+	Duration float(1) NOT NULL CHECK(Duration>=1),
+	EndTime AS DATEADD(hh, Duration*24, StartTime) PERSISTED
 );
 
+GO
+CREATE FUNCTION CheckIsAvailable(@VehicleId AS int, @StartTime as datetime2, @EndTime as datetime2)
+RETURNS bit
+AS
+BEGIN
+	IF (SELECT COUNT(RentId) FROM RentPurchases WHERE VehicleId = @VehicleId AND 
+	NOT (StartTime > @EndTime OR @StartTime > EndTime)) = 1
+		RETURN 1;
+	RETURN 0;
+END
+GO
 
+ALTER TABLE RentPurchases
+ADD CONSTRAINT CheckAvailability
+CHECK([dbo].CheckIsAvailable(VehicleId, StartTime, EndTime)=1);
+
+GO
 CREATE FUNCTION  GetPrice(@StartTime as datetime2, @Duration as float, @SummerPrice as int, @WinterPrice as int)
 RETURNS int
 AS
@@ -81,7 +99,10 @@ BEGIN
 		SET @Duration-=0.5;
 	END
 RETURN @Price
-END;
+END
+GO
+
+------
 
 INSERT INTO Employees(FirstName, LastName, Pin, Salary) VALUES
 ('Ann', 'Cooper', '16633000209', 6260),
@@ -92,35 +113,35 @@ INSERT INTO Employees(FirstName, LastName, Pin, Salary) VALUES
 ('Raymond', 'Anderson', '77052780185', 7630),
 ('Carl', 'Diaz', '65942177266', 7977);
 
-INSERT INTO PriceClasses(ClassName, WinterPrice, SummerPrice) VALUES 
+INSERT INTO PricingClasses(ClassName, WinterPrice, SummerPrice) VALUES 
 ('Coupe', 130, 170),
-('Minivan', 230, 270),
-('Hatchback', 330, 370),
-('SUV', 430, 470),
-('Combi', 540, 590),
-('Moped', 110, 210);
+('Minivan', 100, 160),
+('Hatchback', 150, 200),
+('SUV', 280, 360),
+('Combi', 500, 700),
+('Moped', 50, 80);
 
-INSERT INTO Vehicles(Brand, Model, Type, Color, PriceClassId, Mileage) VALUES 
-('BMW', '840', 'Car', 'Grey', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Coupe'), 2650),
-('BMW', 'M4', 'Car', 'Green', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Coupe'), 2013),
-('Toyota', 'Sienna', 'Car', 'Grey', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Minivan'), 683),
-('Chevrolet', 'Corvette', 'Car', 'Orange', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Coupe'), 1117),
-('Honda', 'Odyssey', 'Car', 'Red', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Minivan'), 2044),
-('Nissan', 'Quest', 'Car', 'Grey', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Minivan'), 1774),
-('Wolkswagen', 'Golf', 'Car', 'Blue', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Hatchback'), 2343),
-('Ford', 'Focus', 'Car', 'Red', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Hatchback'), 1001),
-('Mazda', '3', 'Car', 'Red', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Hatchback'), 2111),
-('Skoda', 'Octavia', 'Car', 'Blue', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Hatchback'), 10774),
-('Wolkswagen', 'GTI', 'Car', 'Black', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Hatchback'), 4455),
-('Skoda', 'Fabia', 'Car', 'Red', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Hatchback'), 1434),
-('Wolkswagen', 'Touareg', 'Car', 'Green', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'SUV'), 2040),
-('Mazda', 'MX-30', 'Car', 'Green', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'SUV'), 9384),
-('Ford', 'Transit', 'Van', 'Black', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Combi'), 3235),
-('Renault', 'Trafic', 'Van', 'Grey', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Combi'), 2968),
-('Suzuki', 'AY50', 'Scooter', 'Yellow', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Moped'), 1784),
-('Piaggio', 'Liberty', 'Scooter', 'Grey', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Moped'), 1555),
-('Piaggio', 'Typhoon', 'Scooter', 'Black', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Moped'), 1246),
-('Citroen', 'Berlingo', 'Van', 'Black', (SELECT PriceClassId FROM PriceClasses WHERE ClassName = 'Combi'), 8246);
+INSERT INTO Vehicles(Brand, Model, Type, Color, PricingClassId, Mileage) VALUES 
+('BMW', '840', 'Car', 'Grey', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Coupe'), 2650),
+('BMW', 'M4', 'Car', 'Green', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Coupe'), 2013),
+('Toyota', 'Sienna', 'Car', 'Grey', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Minivan'), 683),
+('Chevrolet', 'Corvette', 'Car', 'Orange', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Coupe'), 1117),
+('Honda', 'Odyssey', 'Car', 'Red', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Minivan'), 2044),
+('Nissan', 'Quest', 'Car', 'Grey', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Minivan'), 1774),
+('Wolkswagen', 'Golf', 'Car', 'Blue', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Hatchback'), 2343),
+('Ford', 'Focus', 'Car', 'Red', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Hatchback'), 1001),
+('Mazda', '3', 'Car', 'Red', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Hatchback'), 2111),
+('Skoda', 'Octavia', 'Car', 'Blue', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Hatchback'), 10774),
+('Wolkswagen', 'GTI', 'Car', 'Black', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Hatchback'), 4455),
+('Skoda', 'Fabia', 'Car', 'Red', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Hatchback'), 1434),
+('Wolkswagen', 'Touareg', 'Car', 'Green', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'SUV'), 2040),
+('Mazda', 'MX-30', 'Car', 'Green', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'SUV'), 9384),
+('Ford', 'Transit', 'Van', 'Black', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Combi'), 3235),
+('Renault', 'Trafic', 'Van', 'Grey', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Combi'), 2968),
+('Suzuki', 'AY50', 'Scooter', 'Yellow', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Moped'), 1784),
+('Piaggio', 'Liberty', 'Scooter', 'Grey', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Moped'), 1555),
+('Piaggio', 'Typhoon', 'Scooter', 'Black', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Moped'), 1246),
+('Citroen', 'Berlingo', 'Van', 'Black', (SELECT PricingClassId FROM PricingClasses WHERE ClassName = 'Combi'), 8246);
 
 
 INSERT INTO Registrations(VehicleId, RegisterDate) VALUES
@@ -158,18 +179,31 @@ INSERT INTO Registrations(VehicleId, RegisterDate) VALUES
 (20, '2020-5-4');
 
 INSERT INTO RentPurchases (VehicleId, EmployeeId, CustomerFirstName, CustomerLastName, CustomerPin, CustomerBirth, CustomerLicense, CustomerCreditCard, TransactionDate, StartTime, Duration) VALUES
-(5,2,'Richard','Wilson','07649446464','1977-5-4','48066710', '8148489434405333', '2018-4-3', '2018-4-5 18:00', 1.5),
+(5,2,'Richard','Wilson','07649446464','1977-5-4','48066710', '8148489434405333', '2018-4-3', '2018-4-5 19:00', 1.5),
+(2,3,'Anne','Garcia','35710815006','1991-8-23','36218490', '4124471951492068', '2018-4-4', '2018-4-6 22:00', 4),
 (1,5,'Aaron','Howard','56558445316','1988-1-8','46548109', '3099910042133648', '2018-5-3', '2018-5-7 12:00', 2.5),
 (6,7,'Kathy','Ross','06741541992','1954-10-14','99042478', '1926352340788839', '2018-5-6', '2018-5-7 13:00', 1),
 (5,3,'Fred','Bell','45432684014','1992-9-1','94966688', '8372851956355999', '2018-9-30', '2018-9-30 10:00', 4),
+(12,3,'Kathy','Ross','06741541992','1954-10-14','99042478', '1926352340788839', '2018-9-30', '2018-9-30 10:00', 3.5),
+(12,4,'Kathy','Ross','06741541992','1954-10-14','99042478', '1926352340788839', '2018-10-28', '2018-10-30 11:00', 4.5),
 (10,4,'Yudy Young','Wilson','46216788481','2001-2-28','54486082', '6827049039478492', '2019-2-27', '2019-2-28 18:00', 22),
-(17,5,'Timothy','Hughes','48718974899','1995-8-5','80459874', '0465091344030874', '2019-8-1', '2019-8-5 14:00', 1023),
+(17,5,'Timothy','Hughes','48718974899','1995-8-5','80459874', '0465091344030874', '2019-8-1', '2019-8-5 14:00', 32),
 (18,6,'Jeremy','Smith','38333296479','1985-4-19','20763908', '3957665602652717', '2020-1-1', '2020-1-2 9:00', 1.5),
-(5,2,'Benjamin','Johnson','49258947076','1959-12-2','41895417', '1811791766412913', '2020-2-26', '2020-2-27 10:00', 4),
-(4,3,'Anne','Garcia','35710815006','1991-8-23','36218490', '4124471951492068', '2020-10-28', '2020-10-29 18:00', 3),
-(3,1,'Tina','Long','81027056810','1989-11-26','66132530', '8785793173508860', '2020-12-21', '2020-12-24 15:00', 10);
+(19,6,'Jeremy','Smith','38333296479','1985-4-19','20763908', '3957665602652717', '2020-1-27', '2020-2-1 8:00', 2.5),
+(20,3,'Jeremy','Smith','38333296479','1985-4-19','20763908', '3957665602652717', '2020-2-20', '2020-2-27 8:00', 9.5),
+(14,3,'Aaron','Howard','56558445316','1988-1-8','46548109', '2063410332126485', '2020-2-22', '2020-2-27 11:00', 3.5),
+(5,2,'Benjamin','Johnson','49258947076','1959-12-2','41895417', '1811791766412913', '2020-2-26', '2020-2-27 12:00', 4),
+(11,4,'Yudy','Wilson','46216788481','2001-2-28','54486082', '6827049039478492', '2020-2-27', '2020-2-29 18:00', 1),
+(4,3,'Anne','Garcia','35710815006','1991-8-23','36218490', '4124471951492068', '2020-8-28', '2020-8-29 18:00', 3),
+(15,4,'Yudy','Wilson','46216788481','2001-2-28','54486082', '6827049039478492', '2020-10-27', '2020-10-29 19:30', 2.5),
+(2,1,'Aaron','Howard','56558445316','1988-1-8','46548109', '3099910042133648', '2020-10-28', '2020-10-29 21:00', 2),
+(3,1,'Tina','Long','81027056810','1989-11-26','66132530', '8785793173508860', '2020-12-21', '2020-12-24 15:00', 10),
+(16,5,'Fred','Bell','45432684014','1992-9-1','94966688', '8372851956355999', '2020-12-28', '2020-12-30 12:00', 4.5),
+(13,4,'Kathy','Ross','06741541992','1954-10-14','99042478', '1926352340788839', '2020-12-28', '2020-12-29 11:00', 5),
+(18,7,'Kathy','Ross','06741541992','1954-10-14','99042478', '1926352340788839', '2021-2-25', '2021-2-26 11:00', 16),
+(20,3,'Tina','Long','81027056810','1989-11-26','66132530', '8785793173508860', '2021-3-21', '2021-3-24 15:00', 4);
 
-SELECT * FROM Vehicles WHERE (DATEDIFF(yy, LastRegistration, GETDATE())>0);
+SELECT * FROM Vehicles WHERE (DATEDIFF(yy, LastRegistration, GETDATE()) > 0);
 
 SELECT * FROM Vehicles WHERE LastRegistration BETWEEN DATEADD(YEAR,-1, GETDATE()) AND DATEADD(MONTH,-11,GETDATE());
 
@@ -177,52 +211,60 @@ SELECT Type, COUNT(VehicleId) AS NumberOfVehicles FROM Vehicles
 GROUP BY Type
 ORDER BY NumberOfVehicles;
 
-SELECT TOP(5) emp.FirstName, emp.LastName, rts.* FROM RentPurchases rts
-INNER JOIN Employees emp ON emp.EmployeeId = rts.EmployeeId
-WHERE rts.EmployeeId = 2
+SELECT TOP 5 emp.FirstName, emp.LastName, rp.* FROM RentPurchases rp
+JOIN Employees emp ON emp.EmployeeId = rp.EmployeeId
+WHERE rp.EmployeeId = 3
 ORDER BY TransactionDate DESC;
 
-SELECT RentPurchases.StartTime, RentPurchases.Duration, PriceClasses.SummerPrice, PriceClasses.WinterPrice,
-[dbo].GetPrice(RentPurchases.StartTime, RentPurchases.Duration, PriceClasses.SummerPrice, PriceClasses.WinterPrice) AS Price 
-FROM RentPurchases
-JOIN Vehicles ON RentPurchases.VehicleId = Vehicles.VehicleId
-JOIN PriceClasses ON PriceClasses.PriceClassId = Vehicles.PriceClassId
-WHERE RentPurchases.RentId = 3;
+SELECT rp.StartTime, rp.Duration, pc.SummerPrice, pc.WinterPrice,
+[dbo].GetPrice(rp.StartTime, rp.Duration, pc.SummerPrice, pc.WinterPrice) AS Price 
+FROM RentPurchases rp
+JOIN Vehicles v ON v.VehicleId = rp.VehicleId
+JOIN PricingClasses pc ON pc.PricingClassId = v.PricingClassId
+WHERE rp.RentId = 3;
 
 SELECT MIN(CustomerFirstName) AS FirstName, MIN(CustomerLastName) AS LastName, MIN(CustomerBirth) AS Birth,
 MIN(CustomerPin) AS Pin, MIN(CustomerLicense) AS License, MIN(CustomerCreditCard) AS CreditCard  FROM RentPurchases
 GROUP BY CustomerPin;
 
-SELECT MIN(FirstName) AS FirstName, MIN(LastName) AS LastName, MAX(TransactionDate) AS LastTransaction FROM RentPurchases
-JOIN Employees ON RentPurchases.EmployeeId = Employees.EmployeeId
-GROUP BY RentPurchases.EmployeeId;
+SELECT MIN(FirstName) AS FirstName, MIN(LastName) AS LastName, MAX(TransactionDate) AS LastTransaction FROM RentPurchases rp
+JOIN Employees e ON rp.EmployeeId = e.EmployeeId
+GROUP BY rp.EmployeeId;
 
 SELECT MIN(Brand) AS Brand, COUNT(VehicleId) AS NumberOfVehicles FROM Vehicles 
 GROUP BY Brand 
 ORDER BY NumberOfVehicles;
 
-SELECT RentPurchases.*,
-[dbo].GetPrice(RentPurchases.StartTime, RentPurchases.Duration, PriceClasses.SummerPrice, PriceClasses.WinterPrice) AS Price
+SELECT rp.*,
+[dbo].GetPrice(rp.StartTime, rp.Duration, pc.SummerPrice, pc.WinterPrice) AS Price
 INTO RentPurchasesArchive
-FROM RentPurchases
-JOIN Vehicles ON RentPurchases.VehicleId = Vehicles.VehicleId
-JOIN PriceClasses ON PriceClasses.PriceClassId = Vehicles.PriceClassId
-WHERE DATEADD(hh, RentPurchases.Duration*24, RentPurchases.StartTime) < GETDATE();
-DELETE FROM RentPurchases WHERE DATEADD(hh, RentPurchases.Duration*24, RentPurchases.StartTime) < GETDATE();
+FROM RentPurchases rp
+JOIN Vehicles v ON v.VehicleId = rp.VehicleId
+JOIN PricingClasses pc ON pc.PricingClassId = v.PricingClassId
+WHERE DATEADD(hh, rp.Duration*24, rp.StartTime) < GETDATE();
+DELETE FROM RentPurchases WHERE DATEADD(hh, Duration*24, RentPurchases.StartTime) < GETDATE();
 
-SELECT MONTH(TransactionDate) AS Month, COUNT(RentId) AS RentPurchasesCount FROM RentPurchases 
-WHERE YEAR(TransactionDate)=2020
+WITH rp AS (
+SELECT TransactionDate FROM RentPurchases
+UNION ALL
+SELECT TransactionDate FROM RentPurchasesArchive)
+SELECT MONTH(TransactionDate) AS Month, COUNT(TransactionDate) AS RentPurchasesCount FROM rp
+WHERE YEAR(TransactionDate) = 2020
 GROUP BY MONTH(TransactionDate);
 
 SELECT *, CASE
-	WHEN LastRegistration <= DATEADD(MONTH,-11,GETDATE()) THEN 'Treba registraciju'
+	WHEN LastRegistration <= DATEADD(MONTH,-11,GETDATE()) OR LastRegistration IS NULL THEN 'Treba registraciju'
 	ELSE 'Ne treba registraciju'
 	END AS NeedsRegistration
 FROM Vehicles
 WHERE Type = 'Car'
 ORDER BY LastRegistration;
 
-SELECT MIN(Vehicles.Type) AS Type, COUNT(RentId) AS RentAboveAvgCount FROM RentPurchases
-JOIN Vehicles ON RentPurchases.VehicleId = Vehicles.VehicleId
-WHERE Duration > (SELECT CAST(AVG(Duration) AS float) FROM RentPurchases)
-GROUP BY Vehicles.Type;
+WITH rp AS (
+SELECT VehicleId, Duration FROM RentPurchases 
+UNION ALL
+SELECT VehicleId, Duration FROM RentPurchasesArchive)
+SELECT MIN(v.Type) AS Type, COUNT(rp.VehicleId) AS RentAboveAvgCount FROM Vehicles v
+JOIN rp ON rp.VehicleId = v.VehicleId
+WHERE Duration > (SELECT CAST(AVG(Duration) AS float) FROM rp)
+GROUP BY v.Type;
